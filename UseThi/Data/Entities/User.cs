4610669e7ad42120bb58818308163bfd2ba586c4
Kholod13 <111706893+Kholod13@users.noreply.github.com>
2024-04-20 22:29:07 +0200

    create Database

---
+++ UseThi/Data/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace UseThi.Data.Entities
+{
+    public class User : IdentityUser
+    {
+        // add custom properties
+        public DateTime? Birthdate { get; set; }
+    }
+}
--- UseThi/Data/ShopDbContext.cs
+++ UseThi/Data/ShopDbContext.cs
@@ -1,9 +1,54 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UseThi.Data.Entities;
 
 namespace UseThi.Data
 {
-    public class ShopDbContext : DbContext
+    public class ShopDbContext : IdentityDbContext<User>
     {
+        public ShopDbContext()
+        {
 
+        }
+
+        public ShopDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            var connStr = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=web-app-UseThi;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+            optionsBuilder.UseSqlServer(connStr);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>().Property(x => x.Quantity).HasDefaultValue(0);
+
+            modelBuilder.Entity<Category>().HasData(new[]
+            {
+                new Category() { Id = 1, Name = "Electronics" },
+                new Category() { Id = 2, Name = "Sport" },
+                new Category() { Id = 3, Name = "Fashion" },
+                new Category() { Id = 4, Name = "Home & Garden" },
+                new Category() { Id = 5, Name = "Transport" },
+                new Category() { Id = 6, Name = "Toys & Hobbies" },
+                new Category() { Id = 7, Name = "Musical Instruments" },
+                new Category() { Id = 8, Name = "Art" },
+                new Category() { Id = 9, Name = "Other" }
+            });
+
+            modelBuilder.Entity<Product>().HasData(new[]
+            {
+                new Product() { Id = 1, Name = "iPhone X", CategoryId = 1, Discount = 10, Price = 650, ImageUrl = "https://applecity.com.ua/image/cache/catalog/0iphone/ipohnex/iphone-x-black-1000x1000.png" },
+                new Product() { Id = 3, Name = "Nike T-Shirt", CategoryId = 3, Discount = 15, Price = 189, InStock = true, ImageUrl = "https://www.seekpng.com/png/detail/316-3168852_nike-air-logo-t-shirt-nike-t-shirt.png" },
+                new Product() { Id = 4, Name = "Samsung S23", CategoryId = 1, Discount = 5, Price = 1200, InStock = true, ImageUrl = "https://sota.kh.ua/image/cache/data/Samsung-2/samsung-s23-s23plus-blk-01-700x700.webp" },
+                new Product() { Id = 6, Name = "MacBook Pro 2019", CategoryId = 1, Discount = 10, InStock = true, Price = 700, ImageUrl = "https://newtime.ua/image/import/catalog/mac/macbook_pro/MacBook-Pro-16-2019/MacBook-Pro-16-Space-Gray-2019/MacBook-Pro-16-Space-Gray-00.webp" }
+            });
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
     }
 }
